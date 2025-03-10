ea3edb3ea38b3191095569412714d0bc361405c7
Philip Röttgen <58749541+proettgen@users.noreply.github.com>
2025-03-10 22:40:47 +0100

    shorten CardManagerTemplate URL

--- Frontend/app/CardManagerPage/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import CardManagerTemplate from "../../src/components/templates/CardManagerTemplate/index";
-
-function CardManagerPage() {
-  return <CardManagerTemplate />;
-}
-
-export default CardManagerPage;
