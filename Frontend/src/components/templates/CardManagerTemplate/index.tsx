c6a28bfa9e034e8acf1bdf6499d7c9932873c723
Bastian Baulmann <bastianbaulmann@web.de>
2025-03-12 22:13:42 +0100

    fixed CardManager PAge

--- Frontend/src/components/templates/CardManagerTemplate/index.tsx
+++ Frontend/src/components/templates/CardManagerTemplate/index.tsx
@@ -7,12 +7,9 @@ import * as SC from "./styles";
 export default function CardManagerTemplate() {
   return (
     <SC.Container>
-<<<<<<< HEAD
-=======
       <SC.Header>
         <SC.Title>Karteikarten Verwaltung</SC.Title>
       </SC.Header>
->>>>>>> d8fadaade1b237addfe94b24724f4c8c194e2406
       <CardManager />
     </SC.Container>
   );
