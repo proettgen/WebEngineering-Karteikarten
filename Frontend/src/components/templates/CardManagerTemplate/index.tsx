f9cb3727be12b461b8307f5716039dd6a8a080f9
Philip Röttgen <58749541+proettgen@users.noreply.github.com>
2025-03-13 08:30:22 +0100

    Delete header in cards

--- Frontend/src/components/templates/CardManagerTemplate/index.tsx
+++ Frontend/src/components/templates/CardManagerTemplate/index.tsx
@@ -7,9 +7,6 @@ import * as SC from "./styles";
 export default function CardManagerTemplate() {
   return (
     <SC.Container>
-      <SC.Header>
-        <SC.Title>Karteikarten Verwaltung</SC.Title>
-      </SC.Header>
       <CardManager />
     </SC.Container>
   );
