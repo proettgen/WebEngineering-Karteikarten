c7f83c63e04314c772bfa3106388f34b081aadb9
Philip Röttgen <58749541+proettgen@users.noreply.github.com>
2025-04-07 12:15:00 +0200

    adjust base path

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-type NodeEnvType = { BASE_PATH?: "gh-pages" | string };
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv extends NodeEnvType {}
-  }
-}
-
 const nextConfig: NextConfig = {
-  basePath: process.env.BASE_PATH,
+  basePath: "/WebEngineering-Karteikarten",
   output: "export",
   reactStrictMode: true,
   images: { unoptimized: true },
