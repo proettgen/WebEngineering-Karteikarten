771a64843841c7a9832dc6f05395eed785e270c6
Philip Röttgen <58749541+proettgen@users.noreply.github.com>
2025-04-08 16:51:45 +0200

    adjust footer and add github link

--- Frontend/src/components/organisms/Footer/index.tsx
+++ Frontend/src/components/organisms/Footer/index.tsx
@@ -1,12 +1,23 @@
 import * as SC from "./styles";
+import Link from "@/components/atoms/Link";
+import Icon from "@/components/atoms/Icon";
 
 function Footer() {
   return (
     <SC.FooterContainer>
-      <strong>Footer</strong>
+      ´
+      <Link href={"https://github.com/proettgen/WebEngineering-Karteikarten"}>
+        <SC.FooterItem>
+          <Icon size="m" color="textPrimary">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+              <path d="M320-240 80-480l240-240 57 57-184 184 183 183-56 56Zm320 0-57-57 184-184-183-183 56-56 240 240-240 240Z" />
+            </svg>
+          </Icon>{" "}
+          <div>Github</div>
+        </SC.FooterItem>
+      </Link>
     </SC.FooterContainer>
   );
 }
 
 export default Footer;
-
--- Frontend/src/components/organisms/Footer/styles.ts
+++ Frontend/src/components/organisms/Footer/styles.ts
@@ -3,9 +3,27 @@ import styled from "styled-components";
 export const FooterContainer = styled.footer`
   background-color: ${({ theme }) => theme.surface};
   color: ${({ theme }) => theme.textPrimary};
-  padding: 20px;
+  height: 60px;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   border-top: 1px solid ${({ theme }) => theme.border};
+`;
+export const FooterItem = styled.div`
+  display: flex;
+  height: 60px;
+  padding: 0 1rem;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  cursor: pointer;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+    background-color: ${({ theme }) => `${theme.primary}20`};
+    div svg {
+      cursor: pointer;
+      fill: ${({ theme }) => theme.primary};
+    }
+  }
+  transition: all 0.2s ease-in-out;
 `;
