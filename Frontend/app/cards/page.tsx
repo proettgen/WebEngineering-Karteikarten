5c95c0e122587ca937f0c5f838b89d7123834cc6
Philip Röttgen <58749541+proettgen@users.noreply.github.com>
2025-04-10 04:16:31 +0200

    rename links

---
+++ Frontend/app/cards/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+import CardManagerTemplate from "@/components/templates/CardManagerTemplate/index";
+
+function Cards() {
+  return <CardManagerTemplate />;
+}
+
+export default Cards;
---
+++ Frontend/app/test/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import TestTemplate from "../../src/components/templates/TestTemplate/TestTemplate";
+
+//Definiert die Seite unter der Route /Test
+
+//siehe auch Home/page.tsx für erweiterte Erklärung
+
+function Test() {
+  return <TestTemplate />;
+}
+
+export default Test;
