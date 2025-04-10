d3867e282b7c167e0c152d14711a64390148889a
Philip Röttgen <58749541+proettgen@users.noreply.github.com>
2025-04-10 12:31:19 +0200

    Fix casing of /home and /test route

--- Frontend/app/Cards/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import CardManagerTemplate from "@/components/templates/CardManagerTemplate/index";
-
-function Cards() {
-  return <CardManagerTemplate />;
-}
-
-export default Cards;
